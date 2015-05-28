c1bf432bd5af309ad07e5e7b2132648db51e4d1a
tranhv <huynhvantran@gmail.com>
2015-05-28 23:18:48 +0700

    commit code

--- ck/app/controllers/home_controller.rb
+++ ck/app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 	def index
-    @data_companies = Company.all
+	@san = [["Tất cả",1],["HOSE",2],["HNX",3]]
+    @data_companies = Company.where("ma_ck in (select ma_ck from company_businesses where ma_nganh =1)")
 	end
 end
